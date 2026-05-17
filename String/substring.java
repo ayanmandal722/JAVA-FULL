458732649b413e8c598e80e957c699775b223800
Ayan Mandal <ayanmandal722102@gmail.com>
2026-05-18 03:37:44 +0530

    String

---
+++ String/substring.java
@@ -0,0 +1,11 @@
+package String;
+
+public class substring {
+
+    public static void main(String[] args) {
+        String str = "Hello World";
+
+        System.out.println(str.substring(0,7));
+    }
+    
+}

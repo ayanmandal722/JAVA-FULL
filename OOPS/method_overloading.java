1aa82267b46872547a76fd99df786c9d1132ff75
Ayan Mandal <ayanmandal722102@gmail.com>
2026-06-30 06:56:50 +0530

    method overloading

---
+++ OOPS/method_overloading.java
@@ -0,0 +1,26 @@
+//same class name but different parameter type or count
+
+public class method_overloading {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+        System.out.println(calc.sum(4,5));
+        
+        System.out.println(calc.sum(4,5,6));
+
+        System.out.println(calc.sum((float)4.5 , (float)5.6));
+}
+}
+class Calculator{
+    int sum(int a ,int b){
+        return a + b;
+    }
+
+    int sum(int a ,int b ,int c){
+        return a + b +c;
+    }
+
+    float sum(float a ,float b){
+        return a + b ;
+    }
+}

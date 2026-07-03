31763dd457cd09d95f27493034689c47a125c97a
Ayan Mandal <ayanmandal722102@gmail.com>
2026-07-03 17:19:10 +0530

    decreasing order

---
+++ Recursion/decreasing_order.java
@@ -0,0 +1,23 @@
+//use recursion to print in decreasing order
+
+
+package Recursion;
+
+public class decreasing_order {
+
+    public static void decreasing(int n){
+
+        if(n == 1){
+            System.out.println(n);
+            return;
+        }
+
+        System.out.println(n);
+        decreasing(n-1);
+
+    }
+    public static void main(String[] args) {
+        int n = 10;
+        decreasing(n);
+    }
+}

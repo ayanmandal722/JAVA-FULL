4e8dbff48c5075dbdfbe65edcb7a5af347a8231c
Ayan Mandal <ayanmandal722102@gmail.com>
2026-07-14 13:25:10 +0530

    sum_of_natural_numbers

---
+++ Recursion/sum_of_natural_numbers.java
@@ -0,0 +1,17 @@
+package Recursion;
+
+public class sum_of_natural_numbers {
+    public static void main(String args[]){
+        int num = 3;
+        System.out.println(sum(num));
+    }
+    
+    public static int sum(int num){
+        int sum = 0;
+        if(num <= 0){
+            return 0;
+        }
+        return num + sum(num-1);
+        
+    }
+}

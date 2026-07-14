fa7bb606e140ef5e727cc7c1518a1ae063b8028c
Ayan Mandal <ayanmandal722102@gmail.com>
2026-07-14 13:13:33 +0530

    factorial.java

---
+++ Recursion/factorial.java
@@ -0,0 +1,18 @@
+package Recursion;
+
+public class factorial {
+
+    public static void main(String[] args) {
+        int n = 3;
+        System.out.println(fact(n));
+    }
+
+    public static int fact(int n){
+        if(n<=1){
+            return 1;
+        }
+        int result = n * fact(n-1);
+        return result;
+    }
+    
+}
